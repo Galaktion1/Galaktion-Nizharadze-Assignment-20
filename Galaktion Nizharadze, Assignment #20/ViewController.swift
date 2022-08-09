bbe63099d46ffdf927280d6759d6564d68c61780
Gaga <galaqtionn@gmail.com>
2022-08-09 12:58:14 +0400

    initial commit

--- Galaktion Nizharadze, Assignment #20/ViewController.swift
+++ Galaktion Nizharadze, Assignment #20/ViewController.swift
@@ -11,9 +11,43 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
+    
+    @IBAction func calcPrimeNumberButtonAction(_ sender: UIButton) {
+    
+        let primeQueue = DispatchQueue(label: "perform_primeNumbs")
+        sender.isEnabled = false
+        
+        primeQueue.async {
+            self.printPrimeNumbers()
+            DispatchQueue.main.async {
+                sender.isEnabled = true
+            }
+        }
+    }
+    
+    
+    private func printPrimeNumbers() {
+            print(2)
+            print(3)
+            for each in stride(from: 5, through: 10000, by: 2) {
+                if each.isPrime() {
+                    print(each)
+                }
+            }
+    }
 }
 
+
+
+extension Int {
+    func isPrime() -> Bool  {
+        for each in 2 ... self / 2 {
+            if self % each == 0 {
+                return false
+            }
+        }
+        return true
+    }
+}
